c9ac28a6d85ea6214ff61846c5628f83424e4f67
daisy035510 <43841944+daisy035510@users.noreply.github.com>
2023-04-13 13:57:24 +0900

    OOP 학점계산기

---
+++ src/test/java/org/exmaple/CourseTest.java
@@ -0,0 +1,15 @@
+package org.exmaple;
+
+import org.example.Course;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class CourseTest {
+    @DisplayName("과목(코스)을 생성한다")
+    @Test
+    void createTest() {
+        assertThatCode(() -> new Course("OOP", 3, "A+"))
+                .doesNotThrowAnyException();
+    }
+}
